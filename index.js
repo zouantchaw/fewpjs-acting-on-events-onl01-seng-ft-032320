358f6a854bb7370159ef0c460abb60c1605a2ceb
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-06-24 16:13:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+let moveDodgerLeft = () => {
+    let dodger = document.getElementById("dodger")
+    let leftPosition = parseInt(dodger.style.left.replace("px", ""))
+    if(leftPosition > 0) {
+      dodger.style.left = `${leftPosition - 4}px`
+    }
+  }
+  
+  let moveDodgerRight = () => {
+    let dodger = document.getElementById("dodger")
+    let leftPosition = parseInt(dodger.style.left.replace("px", ""))
+    if(leftPosition <= 360) {
+      dodger.style.left = `${leftPosition + 4}px`
+    }
+  }
